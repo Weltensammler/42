7a63c13b8c82d0bf677c9355c8217dff01f31720
Weltensammler <79635532+Weltensammler@users.noreply.github.com>
2021-12-19 21:15:29 +0100

    new

---
+++ minitalk/client/client.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bschende <bschende@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/19 21:07:23 by bschende          #+#    #+#             */
+/*   Updated: 2021/12/19 21:11:14 by bschende         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	value;
+	int	sign;
+
+	i = 0;
+	value = 0;
+	sign = 1;
+	while (*(str + i) == ' ' || (*(str + i) >= 9 && *(str + i) <= 13))
+		i++;
+	if (*(str + i) == '-' || *(str + i) == '+')
+		sign = 1 - 2 * (str[i++] == '-');
+	else if (*str == '\0')
+		return (0);
+	while (ft_isdigit(*(str + i)))
+		value = value * 10 + *(str + i++) - '0';
+	return ((int)(sign * value));
+}
+
+void	send_message(int pid, char *string)
+{
+	int		i;
+	int		j;
+	char	c;
+
+	i = 0;
+	while (*(string + i))
+	{
+		c = *(string + i);
+		j = 0;
+		while (j < 7)
+		{
+			if ((c & 1) == 0)
+			{
+				kill(pid, SIGUSR1);
+				usleep(100);
+			}
+			if ((c & 1) == 1)
+			{
+				kill(pid, SIGUSR2);
+				usleep(100);
+			}
+			c = c >> 1;
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc != 3)
+	{
+		write(1, "Invalid Arguments!\n./client [PID] [STR]\n", 40);
+		exit(0);
+	}
+	pid = ft_atoi(argv[1]);
+	send_message(pid, argv[2]);
+	return (0);
+}
---
+++ minitalk/server/server.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bschende <bschende@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/19 21:07:32 by bschende          #+#    #+#             */
+/*   Updated: 2021/12/19 21:08:28 by bschende         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <signal.h>
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	arr[11];
+	int		i;
+	char	mop;
+
+	i = 0;
+	mop = '+';
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	if (n == -2147483648)
+		return ;
+	if (n < 0)
+	{
+		mop = '-';
+		n = n * -1;
+	}
+	while (n >= 10)
+	{
+		arr[i++] = (n % 10) + '0';
+		n = n / 10;
+	}
+	arr[i] = n + '0';
+	if (mop == '-')
+		write(fd, &mop, 1);
+	while (i >= 0)
+		write(fd, &arr[i--], 1);
+}
+
+void	receive(int signum)
+{
+	static unsigned char	k = '\0';
+	static int				i = 0;
+
+	if (signum == SIGUSR1)
+		i++;
+	if (signum == SIGUSR2)
+	{
+		if (i != 7)
+			k += 1 << i;
+		i++;
+	}
+	if (i == 7)
+	{
+		write(1, &k, 1);
+		if (k == '\0')
+			write(1, "\n", 1);
+		i = 0;
+		k = '\0';
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int pid;
+	char *pidchar;
+
+	pid = getpid();
+	ft_putnbr_fd(pid, 1);
+	write(1, "\n", 1);
+	signal(SIGUSR1, receive);
+	signal(SIGUSR2, receive);
+	while (1)
+		pause();
+	return (0);
+}
