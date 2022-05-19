ecbe736c43e2338aed7899363c611a55d0bed861
Weltensammler <79635532+Weltensammler@users.noreply.github.com>
2022-05-19 15:11:24 +0200

    last race

--- new_beginning/philo/cycle.c
+++ new_beginning/philo/cycle.c
@@ -6,7 +6,7 @@
 /*   By: bschende <bschende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 11:06:26 by bschende          #+#    #+#             */
-/*   Updated: 2022/05/19 15:00:38 by bschende         ###   ########.fr       */
+/*   Updated: 2022/05/19 15:10:08 by bschende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ void	*cycle(t_philid *varsid)
 		thinking(varsid);
 		// if (varsid->vars->stop == 1)
 		// 	return (NULL);
-		pthread_mutex_lock(&varsid->vars->check);
+		pthread_mutex_lock(&varsid->vars->all);
 		if (varsid->vars->stop == 1)
 		{
-			pthread_mutex_unlock(&varsid->vars->check);
+			pthread_mutex_unlock(&varsid->vars->all);
 			break ;
 		}
-		pthread_mutex_unlock(&varsid->vars->check);
+		pthread_mutex_unlock(&varsid->vars->all);
 	}
 	return (NULL);
 }
